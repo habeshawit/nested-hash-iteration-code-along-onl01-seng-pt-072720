adfdfaeb3abbcd88a3884121992f9c81d7e5af9e
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-07 02:32:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
   binding.pry
-end
+  end
 end
 
 contacts.each do |person, contact_details_hash|
@@ -26,4 +26,5 @@ contacts.each do |person, contact_details_hash|
       binding.pry
     end
   end
+end
 end
